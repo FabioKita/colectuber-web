e67e905dfae08c49e1f1c72bcb293ec9cd0b1861
DanielMatsuura <85190874+DanielMatsuura@users.noreply.github.com>
2022-05-25 13:52:02 -0400

    Comit 2

--- components/loading.jsx
+++ components/loading.jsx
@@ -1,16 +1,39 @@
 import React from "react"
 import styles from 'styles/loading.module.scss'
+import {FaBusAlt} from 'react-icons/fa'
+
 const Loading = () =>{
 
 
     return (
         <div className={styles.body}>
             <div className={styles.load}>
-                <img src="public/markers/colectivo/colectivo-red.svg"></img>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load1}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load2}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load3}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load4}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load5}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
+            </div>
+            <div className={styles.load6}>
+                <FaBusAlt size={30} color='white'></FaBusAlt>
             </div>
 
+
+
+
             <div className={styles.text}>
-                <h1>Loading...</h1>
+                <h1>LOADING...</h1>
             </div>
 
 
