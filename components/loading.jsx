0ec51db4399ac417e75443ca1ea8ec6bba5ee57c
DanielMatsuura <85190874+DanielMatsuura@users.noreply.github.com>
2022-05-26 11:40:08 -0400

    CLE-87-Casi Terminado

--- components/loading.jsx
+++ components/loading.jsx
@@ -3,40 +3,33 @@ import styles from 'styles/loading.module.scss'
 import {FaBusAlt} from 'react-icons/fa'
 
 const Loading = () =>{
-
-
     return (
         <div className={styles.body}>
             <div className={styles.load}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load1}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load2}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load3}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load4}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load5}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             <div className={styles.load6}>
-                <FaBusAlt size={30} color='white'></FaBusAlt>
+                <FaBusAlt color='white'></FaBusAlt>
             </div>
             
-
-
-
             <div className={styles.text}>
                 <h1>LOADING...</h1>
             </div>
-
-
         </div>
 
     )
